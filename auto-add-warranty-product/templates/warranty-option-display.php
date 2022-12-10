3c52f1b37baf37e57eea31204c8b6ee935579977
Muhammad Awais <mawais@updraftplus.com>
2022-12-10 16:20:17 +0500

    update text

--- auto-add-warranty-product/templates/warranty-option-display.php
+++ auto-add-warranty-product/templates/warranty-option-display.php
@@ -8,14 +8,14 @@ namespace autoAddWarrantyProduct;
 </div>
 <div class="warranty-text">
 	<div class="warranty-heading">
-		True Lifetime Warranty
+		TORSION-FLEX Lifetime Warranty
 	</div>
 	<div class="warranty-subtext">
-		Warranties the rubber components and connecting hardware for as long as your own your hitch. 
+		Assures the rubber components and connecting hardware for as long as you own your hitch. 
 	</div>
 </div>
 <div class="warranty-checkbox">
 	<input type="checkbox" name="<?php echo Product::WARRANTY_ALLOWED ?>" value="Yes" />
-	<label>Yes, I would like to add a <strong>Lifetime Warranty</strong></label>
+	<label>Yes, I would like to add a <strong>Lifetime Warranty ($99)</strong></label>
 </div>
 </div>
