b9928d4d0ee26afc7c4f08bfa0e2ce7023cd53d0
Muhammad Awais <mawais@updraftplus.com>
2022-12-10 16:25:32 +0500

    cleanup

--- TemplateLoader.php
+++ TemplateLoader.php
@@ -1,13 +1,13 @@
 <?php
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
 /**
  * Class TemplateLoader
+ * @package WPFactory\DeliveryTime
  */
 
-class TemplateLoader
-{
+class TemplateLoader {
 
 	/**
 	 * Instance to call certain functions globally within the plugin.
@@ -19,9 +19,8 @@ class TemplateLoader
 	/**
 	 * Ensures only one instance is loaded or can be loaded.
 	 */
-	public static function get_instance()
-	{
-		if (is_null(self::$instance)) {
+	public static function get_instance() {
+		if ( is_null( self::$instance ) ) {
 			self::$instance = new self();
 		}
 		return self::$instance;
@@ -34,24 +33,23 @@ class TemplateLoader
 	 * @param  string $template_path
 	 * @param  bool $echo
 	 *
-	 */
-	public function get_template($template_name, $args = array(), $template_path, $echo = false)
-	{
+	*/
+	public function get_template( $template_name, $args = array(), $template_path, $echo = false ) {
 		$output = null;
 
 		$template_path = $template_path . $template_name;
 
-		if (file_exists($template_path)) {
-			extract($args); // @codingStandardsIgnoreLine required for template.
+		if ( file_exists( $template_path ) ) {
+			extract( $args ); // @codingStandardsIgnoreLine required for template.
 
 			ob_start();
 			include $template_path;
 			$output = ob_get_clean();
 		} else {
-			throw new \Exception(__('Specified path does not exist', 'dtw-customization'));
+			throw new \Exception( __( 'Specified path does not exist', 'dtw-customization' ) );
 		}
 
-		if ($echo) {
+		if ( $echo ) {
 			print $output;
 		} else {
 			return $output;
