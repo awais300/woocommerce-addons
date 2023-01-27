e69117b643bd71b4a0296b3a59f61bc8d9644ecd
Muhammad Awais <awais300@gmail.com>
2023-01-28 03:44:42 +0500

    add more module

--- loader.php
+++ loader.php
@@ -10,8 +10,7 @@ The idea of this file to include the folder of your custom work. That's it.
 This would allow to keep all WooCommerce custom function under one place.
 This is specifically created for WooCommerce custom work.
 Note: This file should be included via theme's functions.php
-*/
-
+ */
 
 //common
 include_once('Singleton.php');
@@ -26,5 +25,8 @@ include_once('local-pickup-shipping/init.php');
 include_once('auto-add-warranty-product/init.php');
 include_once('auto-add-products/init.php');
 include_once('custom-attributes-fields/init.php');
-include_once('bogo-popup/init.php');
+//include_once('bogo-popup/init.php');
 include_once('customer-free-shipping/init.php');
+include_once('checkout-clear-cart-button/init.php');
+include_once('geny-customizations/init.php');
+include_once('sale-price/init.php'); 
